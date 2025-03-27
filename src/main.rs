aa4c10c73a5d79c38fbfa5dc2a15821685e7eb16
Martin Kronberger <e12202316@student.tuwien.ac.at>
2025-03-27 21:58:08 +0100

    try to generalize

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,135 @@
-// use std::any::Any;
+use std::path::{Path, PathBuf};
 
-// use charming::{
-//     component::{Axis, Legend},
-//     datatype::Dataset,
-//     df,
-//     element::{AxisType, LineStyle, SymbolSize},
-//     series::{Line, Scatter},
-//     Chart, HtmlRenderer,
-// };
-// use csv::{Reader, ReaderBuilder};
+use charming::{
+    component::Axis,
+    element::{Label, MarkLineDataType, MarkPoint, MarkPointData},
+    series::Line,
+    Chart, ImageRenderer,
+};
+use csv::{Position, ReaderBuilder};
+use polyfit_rs::polyfit_rs;
 
-// fn main() {
-//     let mut renderer = HtmlRenderer::new("test", 1000, 800);
+#[derive(Debug)]
+struct PlasmaParams {
+    electron_temp: f64,
+    electron_density: f64,
+    density: f64,
+}
 
-//     // Create a CSV reader for a tab-separated file
-//     let mut reader = ReaderBuilder::new()
-//         .delimiter(b' ')
-//         .from_path("src/test.csv")
-//         .expect("Could not open file");
+impl PlasmaParams {
+    fn new(i_sp: f64, i_sn: f64, d_i: f64, p: f64) -> Self {
+        let electron_temp = ((i_sp * i_sn) / (i_sp + i_sn)) / d_i;
+        let electron_density = 3e21 * (i_sp + i_sn) / electron_temp.sqrt();
+        let density = electron_density / (2.5e22 * p);
+        Self {
+            electron_temp,
+            electron_density,
+            density,
+        }
+    }
+}
 
-//     // Skip the first two lines (if needed)
-//     let records = reader.records().skip(1);
+fn read_and_plot(filepath: PathBuf, renderer: &mut ImageRenderer) {
+    let mut reader = ReaderBuilder::new()
+        .delimiter(b' ')
+        .from_path(filepath)
+        .expect("Could not open file");
 
-//     // Collect CSV data into a vector of points (assuming two columns per row)
-//     let mut data = Vec::new();
-//     for result in records {
-//         let record = result.expect("Could not parse record");
-//         // Parse the first two columns as f64 values
-//         let x: f64 = record.get(0).unwrap().parse().unwrap_or_default();
-//         let y: f64 = record.get(4).unwrap().parse().unwrap_or_default();
-//         data.push(vec![x, y]);
-//     }
-//     println!("{:?}", data);
+    let records = reader.records().skip(1);
 
-//     // Create a chart using the CSV data
-//     let chart = Chart::new()
-//         .x_axis(Axis::new().align_ticks(true))
-//         .y_axis(Axis::new().align_ticks(true))
-//         .series(Scatter::new().symbol_size(20).data(df!(data)));
+    let mut data = Vec::new();
 
-//     renderer
-//         .save(&chart, "src/plot.html")
-//         .unwrap_or_else(|e| eprintln!("Could not render file: {}", e));
-// }
-use charming::{component::Axis, series::Scatter, Chart, HtmlRenderer};
+    for result in records {
+        let record = result.expect("Could not parse record");
+        // Parse the first two columns as f64 values
+        let x: f64 = record.get(0).unwrap().parse().unwrap_or_default();
+        let y: f64 = record.get(2).unwrap().parse().unwrap_or_default();
+        data.push(vec![x, y]);
+    }
 
-pub fn chart() -> Chart {
-    Chart::new().x_axis(Axis::new()).y_axis(Axis::new()).series(
-        Scatter::new().symbol_size(20).data(vec![
-            vec![10.0, 8.04],
-            vec![8.07, 6.95],
-            vec![13.0, 7.58],
-            vec![9.05, 8.81],
-            vec![11.0, 8.33],
-            vec![14.0, 7.66],
-            vec![13.4, 6.81],
-            vec![10.0, 6.33],
-            vec![14.0, 8.96],
-            vec![12.5, 6.82],
-            vec![9.15, 7.2],
-            vec![11.5, 7.2],
-            vec![3.03, 4.23],
-            vec![12.2, 7.83],
-            vec![2.02, 4.47],
-            vec![1.05, 3.33],
-            vec![4.05, 4.96],
-            vec![6.03, 7.24],
-            vec![12.0, 6.26],
-            vec![12.0, 8.84],
-            vec![7.08, 5.82],
-            vec![5.02, 5.68],
-        ]),
-    )
+    // Get a slice of the tail of the data
+    let tail_slice = &data[500..];
+
+    // Unzip the slice into x and y vectors
+    let (x, y): (Vec<f64>, Vec<f64>) = tail_slice.iter().map(|v| (v[0], v[1])).unzip();
+
+    // Perform the polyfit on the tail slice
+    let fit_tail = polyfit_rs::polyfit(&x, &y, 1).expect("Could not fit the curve!");
+
+    // Compute the fitted values for the tail slice
+    let fit_data_tail: Vec<Vec<f64>> = data
+        .iter()
+        .map(|record| {
+            let x_val = record[0];
+            let y_val = x_val * fit_tail[1] + fit_tail[0];
+            vec![x_val, y_val]
+        })
+        .collect();
+
+    // Get a slice of the front of the data
+    let front_slice = &data[..100];
+
+    // Unzip the slice into x and y vectors
+    let (x_front, y_front): (Vec<f64>, Vec<f64>) = front_slice.iter().map(|v| (v[0], v[1])).unzip();
+
+    // Perform the polyfit on the front slice
+    let fit_front =
+        polyfit_rs::polyfit(&x_front, &y_front, 1).expect("Could not fit the front curve!");
+
+    // Compute the fitted values for the front slice
+    let fit_data_front: Vec<Vec<f64>> = data
+        .iter()
+        .map(|record| {
+            let x_val = record[0];
+            let y_val = x_val * fit_front[1] + fit_front[0];
+            vec![x_val, y_val]
+        })
+        .collect();
+
+    let d_i = match data.iter().find(|v| v[0] == 0.0) {
+        Some(point) => point[1],
+        None => {
+            eprintln!("No point at 0.0 found! Returning 0.0");
+            0.0
+        }
+    };
+
+    let plasma = PlasmaParams::new(fit_front[0], fit_tail[0], d_i, 0.6);
+
+    println!("{:?}", plasma);
+
+    // Create a chart using the CSV data
+    let chart = Chart::new()
+        .x_axis(Axis::new().name("U in V"))
+        .y_axis(Axis::new().name("Ig in muA"))
+        .series(Line::new().show_symbol(false).data(data))
+        .series(
+            Line::new()
+                .show_symbol(false)
+                .data(fit_data_front)
+                .mark_point(MarkPoint::new().data(vec![MarkPointData::new()
+                        .name("Something")
+                        .x_axis(0.0)
+                        .y_axis(fit_front[0]).value((fit_front[0]*10.0).round()/10.0)])),
+        )
+        .series(
+            Line::new()
+                .show_symbol(false)
+                .data(fit_data_tail)
+                .mark_point(MarkPoint::new().data(vec![MarkPointData::new()
+                        .name("Something")
+                        .x_axis(0.0)
+                        .y_axis(fit_tail[0]).value((fit_tail[0]*10.0).round()/10.0)])),
+        );
+
+    renderer
+        .save(&chart, "src/plot.svg")
+        .unwrap_or_else(|e| eprintln!("Could not render file: {}", e));
 }
 
 fn main() {
-    let mut renderer = HtmlRenderer::new("test", 1000, 800);
-    // Create a chart using the CSV data
-    let chart = chart();
-    renderer
-        .save(&chart, "src/plot.html")
-        .unwrap_or_else(|e| eprintln!("Could not render file: {}", e));
+    let mut renderer = ImageRenderer::new(1000, 800);
+    let path = Path::new("data/data_5-2-4.csv").to_path_buf();
+
+    read_and_plot(path, &mut renderer);
 }
